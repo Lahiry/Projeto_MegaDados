435ba9a94e3fbee2bb86f260f707f7e0f34e93a3
RodrigoCoelho <rodrigogc4@al.insper.edu.br>
2022-10-16 18:13:15 -0300

    Update and delete done

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import json
   
-f = open('products.json')
-products = json.load(f)
+f = open('stock.json')
+products = json.load(f)['products']
 
 class Product(BaseModel):
     id: int
@@ -13,26 +13,11 @@ class Product(BaseModel):
 
 app = FastAPI()
 
-# get todos
-# get individual
-# create individual
 # update individual
 # delete individual
 # error handling
 
-@app.get("/products")
-async def get_products():
-    return products["products"]
-
-@app.get("/products/{id_product}")
-async def get_product(id_product: int):
-    for product in products["products"]:
-        if product["id"] == id_product:
-            return product
-    raise HTTPException(status_code=404, detail="Product not found")
-
-@app.post("/products")
-async def create_product(product: Product):
+def verify_restrictions(product: Product, verify_product_exists: bool = True):
     if product.id < 0:
         raise HTTPException(status_code=400, detail="Id cannot be negative")
 
@@ -42,16 +27,69 @@ async def create_product(product: Product):
     if product.price < 0:
         raise HTTPException(status_code=400, detail="Price cannot be negative")
 
-    if product.id in [product["id"] for product in products["products"]] or product.name in [product["name"] for product in products["products"]]:
-        raise HTTPException(status_code=400, detail="Product already exists")
+    if verify_product_exists:
+        if product.id in [product["id"] for product in products["products"]] or product.name in [product["name"] for product in products["products"]]:
+            raise HTTPException(status_code=400, detail="Product already exists")
 
     if product.name == "":
         raise HTTPException(status_code=400, detail="Name cannot be empty")
-    
-    products["products"].append(product.dict())
+
+def save_product(product: Product):
+    products.append(product.dict())
+    with open('products.json', 'w') as f:
+        json.dump(products, f)
+
+
+@app.get("/products")
+async def get_products():
+    return products
+
+@app.get("/product/{id_product}")
+async def get_product(id_product: int):
+    for product in products:
+        if product["id"] == id_product:
+            return product
+    raise HTTPException(status_code=404, detail="Product not found")
+
+@app.post("/product")
+async def create_product(product: Product):
+
+    verify_restrictions(product)
+    save_product(product)
+
+    return "Product created successfully"
+
+@app.patch("/product")
+async def update_product(product: Product):
+
+    verify_restrictions(product, False)
+
+    update_product = product.dict()
+
+    for stock_product in products:
+        if stock_product["id"] == update_product["id"]:
+            stock_product["name"] = update_product["name"]
+            stock_product["price"] = update_product["price"]
+            stock_product["quantity"] = update_product["quantity"]
+            break
 
     with open('products.json', 'w') as f:
         json.dump(products, f)
 
-    return "Product created successfully"
+    return "Product updated successfully"
 
+@app.delete("/product/{id_product}")
+async def delete_product(id_product: int):
+
+    if id_product < 0:
+        raise HTTPException(status_code=400, detail="Id cannot be negative")
+
+    if id_product not in [product["id"] for product in products["products"]]:
+        raise HTTPException(status_code=400, detail="Product doens't exists")
+
+    filtered_products = [product for product in products if product['id'] != id_product]
+
+    with open('products.json', 'w') as f:
+        json.dump(filtered_products, f)
+    
+    return "Product deleted successfully"
